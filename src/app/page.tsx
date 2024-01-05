78808096cfbdf2031009fde15bf967b933ba52f0
TrueMolni <truemolni@gmail.com>
2024-01-05 19:25:39 +0200

    add hover and display hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       <div className="container bg-green-300">
-        <Navbar />
+        <Navbar isMobile={false} />
         <h1 className="text-2xl text-accent">Подарунок</h1>
         <Example />
       </div>
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,18 +1,24 @@
 import { Link } from "react-scroll";
 
 import React from "react";
-import data from "../../lib/navbar.json";
+import data from "../../data/navbar.json";
+type navData = {
+  text: string;
+  textRef: string;
+  id: number;
+};
+type navProps = {
+  isMobile: boolean;
+};
 
-type navProps = { text: string; textRef: string; id: number };
-
-const Navbar = () => {
+const Navbar = ({ isMobile }: navProps) => {
   return (
-    <nav className="flex flex-col  lg:flex-row">
-      <ul className="flex flex-col lg:flex-row gap-6 list-none text-center">
-        {data.map(({ text, textRef, id }: navProps) => (
+    <nav className={!isMobile ? "hidden lg:flex" : "lg:hidden flex "}>
+      <ul className="flex flex-col lg:flex-row gap-6 mx-auto list-none text-center justify-center">
+        {data.map(({ text, textRef, id }: navData) => (
           <li key={id} className="">
             <Link
-              className="font-sans font-normal text-black hover:text-accent focus:text-accent 
+              className="font-sans font-normal text-black transition hover:text-accent focus:text-accent 
               text-3xl lg:text-base"
               to={textRef}
               spy
