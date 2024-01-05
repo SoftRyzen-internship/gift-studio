93e7a46a5bcbbea45ceda5a9b2de3d7b1ae7afb2
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-05 14:44:13 +0200

    remove shadcn and others

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,24 +1,7 @@
 import React from "react";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
 
 function Example() {
-  return (
-    <div className="max-w-md bg-blue-100">
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible??</AccordionTrigger>
-          <AccordionContent className="font-display">
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
-  );
+  return <div className="max-w-md bg-blue-100"></div>;
 }
 
 export default Example;
