4dd7dc0223279db127be11dd428a071a31860446
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-05 14:56:15 +0200

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import Example from "@/components/Example";
-import TestIcon from "@/public/assets/icons/placeholder.svg";
+// import TestIcon from "@/public/assets/icons/placeholder.svg";
 
 export default function Home() {
   return (
     <div className="container bg-green-300">
-      <h1 className="text-2xl leading-25 text-accent">Подарунок!</h1>
-      <TestIcon className="h-[100px] w-[100px] stroke-accent" />
+      <h1 className="text-2xl leading-25 text-accent">Подарунок</h1>
+      {/* <TestIcon className="h-[100px] w-[100px] stroke-accent" /> */}
       <Example />
     </div>
   );
