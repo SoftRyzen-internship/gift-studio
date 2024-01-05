42679406a13a6b22b4594416b26fe75ecb853fa3
Nadiia Khoptiuk <holineinadia@gmail.com>
2024-01-05 17:55:01 +0200

    finish configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@
 import Example from "@/components/Example";
 
 export default function Home() {
-    return (
-        <div className="container bg-green-300">
-            <h1 className="text-2xl text-accent">Подарунок</h1>
-            <Example />
-        </div>
-    );
+  return (
+    <div className="container bg-green-300">
+      <h1 className="text-2xl text-accent">Подарунок</h1>
+      <Example />
+    </div>
+  );
 }
