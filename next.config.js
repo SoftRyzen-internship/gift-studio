569cd22c5f509a885455bcb74d67b05d028dc3fa
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-05 17:38:08 +0200

    add prettier

--- next.config.js
+++ next.config.js
@@ -1,30 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.(".svg"),
-    );
+    webpack(config) {
+        const fileLoaderRule = config.module.rules.find((rule) =>
+            rule.test?.test?.(".svg")
+        );
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
+        config.module.rules.push(
+            {
+                ...fileLoaderRule,
+                test: /\.svg$/i,
+                resourceQuery: /url/, // *.svg?url
+            },
 
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
-        use: ["@svgr/webpack"],
-      },
-    );
+            {
+                test: /\.svg$/i,
+                issuer: fileLoaderRule.issuer,
+                resourceQuery: {
+                    not: [...fileLoaderRule.resourceQuery.not, /url/],
+                }, // exclude if *.svg?url
+                use: ["@svgr/webpack"],
+            }
+        );
 
-    // Modify the file loader rule to ignore *.svg, since we have it handled now.
-    fileLoaderRule.exclude = /\.svg$/i;
+        // Modify the file loader rule to ignore *.svg, since we have it handled now.
+        fileLoaderRule.exclude = /\.svg$/i;
 
-    return config;
-  },
+        return config;
+    },
 };
 
 module.exports = nextConfig;
