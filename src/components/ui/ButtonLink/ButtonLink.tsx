7619f236d174a8f88236f37cab5cdeab3a6fc42e
Oleksandr P <goodwin009@gmail.com>
2024-01-10 13:59:07 +0200

    feat: add about section

--- src/components/ui/ButtonLink/ButtonLink.tsx
+++ src/components/ui/ButtonLink/ButtonLink.tsx
@@ -14,7 +14,7 @@ const ButtonLink: FC<ButtonLinkProps> = ({
     primary:
       "text-white text-base font-bold bg-accent py-4 px-6 rounded-[10px] hover:bg-lava",
     outline:
-      "text-accent text-base font-bold bg-white py-4 px-6 rounded-[10px] hover:bg-accent hover:text-white",
+      "text-accent text-base font-bold bg-white py-4 px-6 border-accent border-[1px] rounded-[10px] hover:bg-accent hover:text-white",
     ghost:
       "font-montserrat text-3xl font-normal text-black hover:text-accent focus:text-accent lg:text-base",
   };
@@ -22,7 +22,7 @@ const ButtonLink: FC<ButtonLinkProps> = ({
   return (
     <Link
       className={cn(
-        "cursor-pointer transition inline-block",
+        "cursor-pointer transition block w-fit",
         styles[variant],
         className,
       )}
--- src/sections/AboutSection/AboutSection.tsx
+++ src/sections/AboutSection/AboutSection.tsx
@@ -1,30 +1,37 @@
 import SectionHeading from "@/components/typography/SectionHeading";
 import ButtonLink from "@/components/ui/ButtonLink";
 import content from "@/data/about.json";
+import Image from "next/image";
 
 const AboutSection = () => {
-  const { title, id } = content;
+  const { id, title, image, description, buttonLabel, btnScrollTo } = content;
 
   return (
-    <section id={id} className="section container">
-      <SectionHeading title={title} />
-
-      {/* REMOVE: ONLY FOR EXAMPLE BUTTONS LINKS */}
-      <div className="mx-auto block space-y-4">
-        <ButtonLink
-          label="Переглянути букети"
-          elementName="FAQ"
-          variant="primary"
+    <section id={id} className="section">
+      <div className="container mx-auto md:max-lg:px-[88px] lg:grid lg:grow lg:grid-cols-2 lg:justify-items-start lg:gap-x-8 lg:gap-y-2">
+        <SectionHeading title={title} className="lg:mb-0 lg:mt-6 lg:self-end" />
+        <Image
+          className="h-[376px] w-full rounded-3xl max-lg:mb-9 max-md:mb-6 md:h-[645px] lg:order-first lg:row-span-3 lg:max-w-[548px]"
+          src={`/assets/images/about/about@x2.webp`}
+          alt={image.alt}
+          width={320}
+          height={376}
+          sizes="(max-width: 767px) 100vw, (min-width: 1280px) 548px"
         />
+        <ul className="max-md:mb-6 md:max-lg:mb-10 lg:h-[448px] lg:overflow-auto">
+          {description.map(text => (
+            <li key={text} className="mb-[21.6px] last:mb-0">
+              <p className="text-base font-light leading-[21.6px] md:text-xl md:font-normal md:leading-27">
+                {text}
+              </p>
+            </li>
+          ))}
+        </ul>
         <ButtonLink
-          label="Переглянути букети"
-          elementName="FAQ"
+          label={buttonLabel}
+          elementName={btnScrollTo}
           variant="outline"
-        />
-        <ButtonLink
-          label="Переглянути букети"
-          elementName="FAQ"
-          variant="ghost"
+          className="max-lg:mx-auto  lg:mb-6 lg:self-start"
         />
       </div>
     </section>
