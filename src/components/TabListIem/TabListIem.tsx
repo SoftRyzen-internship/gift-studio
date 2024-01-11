560647fa9d63a10bd6d2814afdfb005c8dfada86
Oleksandr P <goodwin009@gmail.com>
2024-01-11 18:09:16 +0200

    refactor: update tabListItem:add animation, update desktop font size, fixes by Nadiia comments, fix meta title

--- src/components/TabListIem/TabListIem.tsx
+++ src/components/TabListIem/TabListIem.tsx
@@ -13,32 +13,32 @@ const TabListIem: FC<TabListIemProps> = ({
   setActive,
 }) => {
   const answerWithSocialLink = getSocialLink(answer);
+  const styles = {
+    base: "cursor-pointer rounded-3xl text-left max-md:px-4 max-md:py-6 md:max-lg:p-8 lg:py-8 lg:pl-8 lg:pr-4",
+    question: "text-lg font-bold lg:text-3xl lg:leading-32",
+  };
 
-  return (
+  return !isActive ? (
+    <li onClick={setActive} className={cn(styles.base, "border-none bg-latte")}>
+      <p className={styles.question}>{question}</p>
+    </li>
+  ) : (
     <li
       onClick={setActive}
       className={cn(
-        "flex flex-col text-left gap-1 md:gap-2 transition border-none max-md:px-4 max-md:py-6 md:max-lg:p-8 lg:pr-4 lg:pl-8 lg:py-8 rounded-3xl bg-latte cursor-pointer",
-        { "bg-white border-solid border-[1px] border-accent": isActive },
+        styles.base,
+        "answer-opening flex flex-col gap-1 border-[1px] border-solid border-accent bg-white md:gap-2",
       )}
     >
-      <p
-        className={cn("text-lg font-bold transition", {
-          "text-accent": isActive,
+      <p className={cn(styles.question, "text-accent")}>{question}</p>
+      <p className="text-base font-normal leading-16">
+        {answerWithSocialLink.map((text, index) => {
+          if (index === 1) {
+            return <SocialLink key={index} username={text} />;
+          }
+          return text;
         })}
-      >
-        {question}
       </p>
-      {isActive && (
-        <p className="text-base font-normal leading-16 transition">
-          {answerWithSocialLink.map((text, index) => {
-            if (index === 1) {
-              return <SocialLink key={index} username={text} />;
-            }
-            return text;
-          })}
-        </p>
-      )}
     </li>
   );
 };
