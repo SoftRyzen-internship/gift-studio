209cc0e945affff52dee203143c877b531de1558
Sergio Nezhigay <abc1971h@gmail.com>
2024-01-22 18:15:01 +0200

    add overflow hidden

--- src/sections/About/About.tsx
+++ src/sections/About/About.tsx
@@ -17,7 +17,7 @@ const About = async () => {
           title={title}
           className="md:max-lg:mb-9 lg:mb-0 lg:mt-6 lg:self-end"
         />
-        <div className="relative h-[376px] w-full rounded-3xl max-lg:mb-9 max-md:mb-6  md:h-[645px] lg:order-first lg:row-span-3 lg:max-w-[548px]">
+        <div className="relative h-[376px] w-full overflow-hidden rounded-3xl max-lg:mb-9 max-md:mb-6  md:h-[645px] lg:order-first lg:row-span-3 lg:max-w-[548px]">
           <SanityImage
             image={aboutImage}
             priority
